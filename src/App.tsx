0502df8adb1ff176ee5bdaac9ee69aed5fa8afbf
Emrulla Ramilli <emrulla.ramilli@softup.co>
2024-10-06 10:31:25 +0200

    added router logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+} from "react-router-dom";
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1>Hello World</h1>
+          <Link to="about">About Us</Link>
+        </div>
+      ),
+    },
+    {
+      path: "about",
+      element: <div>About</div>,
+    },
+  ]);
+
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
